7946041ca6aa476bb2514150383b15f5c1dbc7c9
Sergey Chepukhin <chepukhin.sergey@gmail.com>
2023-02-08 20:57:39 +0500

    steps/step-2: decompose webpack config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,25 @@
+import webpack from "webpack";
+
+type WebpackRule = webpack.RuleSetRule;
+
+/**
+ * Get webpack loaders list.
+ *
+ * [Loaders examples](https://webpack.js.org/loaders/#root)
+ */
+export function buildLoaders(): WebpackRule[] {
+
+  /**
+   * [Loader options](https://github.com/TypeStrong/ts-loader#loader-options)
+   * [ts-loader guide](https://webpack.js.org/guides/typescript/#loader)
+   */
+  const typeScriptLoader: WebpackRule = {
+    test: '/\.tsx?$/',
+    use: 'ts-loader',
+    exclude: '/node_modules/',
+  }
+
+  return [
+    typeScriptLoader,
+  ]
+}
