f88ad40d5d4953d3e9bd2b5cc51930c25cd61630
Sergey Chepukhin <chepukhin.sergey@gmail.com>
2025-02-07 22:45:53 +0500

    steps/step-6: routing, code splitting, lazy suspense (#6)
    Co-authored-by: Sergey Chepukhin <sergey.chepukhin@dodobrands.io>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import {Counter} from "./components/Counter";
 import "./app.scss"
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./pages/MainPage/MainPage.lazy";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.lazy";
+import {Suspense} from "react";
 
 
 export const App = () => {
     return (
         <div className="app">
-            <Counter/>
+            <Link to="/">Главная</Link>
+            <Link to="/about">О сайте</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<MainPageAsync/>}/>
+                    <Route path="/about" element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
