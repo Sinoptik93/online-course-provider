4b58e685075ef82e4fbca9f89506015290a7f208
Sergey Chepukhin <chepukhin.sergey@gmail.com>
2025-02-08 11:58:28 +0500

    steps/step-11: router config (#10)
    Co-authored-by: Sergey Chepukhin <sergey.chepukhin@dodobrands.io>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
-import {Suspense} from "react";
-import {Link, Route, Routes} from "react-router-dom";
-import {MainPage} from "pages/MainPage";
-import {AboutPage} from "pages/AboutPage";
+import {Link} from "react-router-dom";
 import {useTheme} from "shared/config/theme";
 import {classNames} from "shared/lib/classNames";
 
 import "./styles/index.scss"
+import {AppRouter} from "app/providers/router";
 
 
 export const App = () => {
@@ -16,13 +14,7 @@ export const App = () => {
             <Link to="/">Главная</Link>
             <Link to="/about">О сайте</Link>
             <button onClick={toggleTheme}>Toggle theme</button>
-
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/" element={<MainPage/>}/>
-                    <Route path="/about" element={<AboutPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
---
+++ src/app/providers/router/index.ts
@@ -0,0 +1,2 @@
+export {AppRouter} from "./ui/AppRouter";
+export {routerConfig} from "./routerConfig";
