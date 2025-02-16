0bc2bb6a308444426172f18e45bfa1d09743a94a
Sergey Chepukhin <chepukhin.sergey@gmail.com>
2025-02-16 17:31:03 +0500

    steps/step-16: add HMR (#15)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,6 @@
-import {useTranslation} from "react-i18next";
 import {AppRouter} from 'app/providers/router';
 import {Navbar} from 'widget/Navbar';
 import {Sidebar} from 'widget/Sidebar';
-import {LangSwitcher} from "widget/LangSwitcher";
 import {useTheme} from 'shared/config/theme';
 import {classNames} from 'shared/lib/classNames';
 
